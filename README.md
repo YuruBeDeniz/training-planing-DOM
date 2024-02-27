7514481e3e29a975ab687d0dad541972447823f3
YuruBeDeniz <saganak.deniz@gmail.com>
2024-02-27 14:01:47 +0100

    feat/update readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # training-planing-DOM
 
 https://yurubedeniz.github.io/training-planing-DOM/
+
+
+## Create Cards
+To create the carts I wrote a createCard function which takes title, description, color, className arguments (depending on what will be shown in the card, this arguments can change)
+
+The cards have a card class for general styling which I assigned it in createCard function and individual class names for individual styling which I added them additionally with the for loop
+
+I wrapped the texts and the icon to be able to use flex box and appended them to the card div
+
+To assign the cards their parameters, i.e., their colors, titles, descriptions and classnames I used getElementById() method and for loop
+
+## Filter Cards
+To filter the checked values to be able to filter cards, I assigned them names (goal and level) & to get all the cards I use querySelectorAll('.card') method
+
+And to be able to easily filter them with their class names (and to be able to use card.style.display = 'flex' || 'none'), I created a goalCardMap object and sorted the class names in line with the input type check. 
+
+Finally, I loop through the checkedValues and assign the value to goalCardMap to be able to return the classNames for the checked value. Once I assigned thoese values to relatedCardClasses, I could be able to filter them from relatedCardClasses and assign it to filteredCardClasses. 
+
+## Style Cards
+To be able to achieve the requested design, I used CSS grid. There are 9 cards -one spanning to the second row while the rest occupies 1 Fr. Therefore, the grid has three columns with 1 fr each. To make it responsive I used media queries; until 768 px the cards are stacked in one column.
--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function createCard(title, description, color, className) {
 };
 
 const mainContent = document.getElementById('main-content');
-const colors = ['#00cbff', '#009fff', '#0055ff', '#ff3799', '#ac00ff', '#00b749', '#ff7f00', '#ffcc00'];
 
+const colors = ['#00cbff', '#009fff', '#0055ff', '#ff3799', '#ac00ff', '#00b749', '#ff7f00', '#ffcc00'];
 const titles = ['Mehr Abwechslung, mehr Erfolg!', 'PURES LESEN', 'RELAX & FOKUS', 'WORTSCHATZ', '20-MINUTEN', 'LEVEL-UP', 'WORTSCHATZ & FOKUS', 'GENAUES LESEN'];
 const descriptions = ['Erhalte hier mehr Übungsempfehlungen…', 'This is the description of Training Plan 2', 'This is the description of Training Plan 3', 'This is the description of Training Plan 4', 'This is the description of Training Plan 5', 'This is the description of Training Plan 6', 'This is the description of Training Plan 7', 'This is the description of Training Plan 8'];
 const classNames = ['info', 'pures-lesen', 'relax-fokus', 'wortschatz', 'zwanzig-minuten', 'level-up', 'wortschatz-fokus', 'genaues-lesen'];
@@ -62,16 +62,18 @@ function filterCards() {
     }
 
     let filteredCardClasses = [];
+   
 
     for (const value of checkedValues) {
         const relatedCardClasses = goalCardMap[value];
-        console.log(relatedCardClasses)
+        console.log("relatedCardClasses: ", relatedCardClasses)
         if (filteredCardClasses.length === 0) {
             filteredCardClasses = relatedCardClasses;
         } else {
             filteredCardClasses = filteredCardClasses.filter(className => relatedCardClasses.includes(className));
         }
     }
+    console.log("filteredCardClasses: ", filteredCardClasses)
 
     const filteredCards = document.querySelectorAll('.card.' + filteredCardClasses.join(', .card.'));
     
