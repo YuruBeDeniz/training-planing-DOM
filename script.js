bdcd2b3a36ee1ae18a2ee58b98489ffd04fc742e
YuruBeDeniz <saganak.deniz@gmail.com>
2024-02-27 15:50:32 +0100

    feat/make niveau input functional and solve responsive issues

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ const mainContent = document.getElementById('main-content');
 
 const colors = ['#00cbff', '#009fff', '#0055ff', '#ff3799', '#ac00ff', '#00b749', '#ff7f00', '#ffcc00'];
 const titles = ['Mehr Abwechslung, mehr Erfolg!', 'PURES LESEN', 'RELAX & FOKUS', 'WORTSCHATZ', '20-MINUTEN', 'LEVEL-UP', 'WORTSCHATZ & FOKUS', 'GENAUES LESEN'];
-const descriptions = ['Erhalte hier mehr Übungsempfehlungen…', 'This is the description of Training Plan 2', 'This is the description of Training Plan 3', 'This is the description of Training Plan 4', 'This is the description of Training Plan 5', 'This is the description of Training Plan 6', 'This is the description of Training Plan 7', 'This is the description of Training Plan 8'];
+const descriptions = ['Erhalte hier mehr Übungsempfehlungen aus über 10 Jahren Erfahrung und aus mehr als 25.000 virtuellen Klassenstunden. Du verfolgst mehrere Ziele? Ergäanze dein Förderprogramm bis zu 18 Wochen im Voraus mit wenigen Klicks.',
+ 'Lesekompetenz | Schwer | 3 Wochen', 'Konzentration | Leicht | 3 Wochen', 'Neue Wörter | Leicht | 3 Wochen', 'Lesekompetenz | Mittel | 6 Wochen', 'Lesekompetenz | Mittel | 6 Wochen', 'Lesekompetenz | Mittel | 6 Wochen', 'Textversändnis | Scwher | 3 Wochen'];
 const classNames = ['info', 'pures-lesen', 'relax-fokus', 'wortschatz', 'zwanzig-minuten', 'level-up', 'wortschatz-fokus', 'genaues-lesen'];
 
 for (let i = 0; i < titles.length; i++) {
@@ -46,7 +47,7 @@ const goalCardMap = {
 
 const goalCheckboxes = document.querySelectorAll('input[name="goal"]');
 
-function filterCards() {
+function filterGoalCards() {
     const checkedValues = [...goalCheckboxes].filter(checkbox => checkbox.checked).map(e => e.value);
     const allCards = document.querySelectorAll('.card');
 
@@ -58,6 +59,9 @@ function filterCards() {
         allCards.forEach(card => {
             card.style.display = 'flex';
         });
+
+        const planingAmount = document.getElementById('plan-amount');
+        planingAmount.innerHTML = '';
         return;
     }
 
@@ -66,23 +70,85 @@ function filterCards() {
 
     for (const value of checkedValues) {
         const relatedCardClasses = goalCardMap[value];
-        console.log("relatedCardClasses: ", relatedCardClasses)
+
         if (filteredCardClasses.length === 0) {
             filteredCardClasses = relatedCardClasses;
         } else {
             filteredCardClasses = filteredCardClasses.filter(className => relatedCardClasses.includes(className));
         }
     }
-    console.log("filteredCardClasses: ", filteredCardClasses)
 
     const filteredCards = document.querySelectorAll('.card.' + filteredCardClasses.join(', .card.'));
     
     filteredCards.forEach(card => {
         card.style.display = 'flex';
     });
+
+    const planingAmount = document.getElementById('plan-amount');
+    planingAmount.innerHTML = `<span>(${filteredCards.length})</span>`;
 }
 
 goalCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', filterCards);
+    checkbox.addEventListener('change', filterGoalCards);
 });
+
+
+const levelCardMap = {
+    leicht: ['relax-fokus', 'wortschatz'],
+    mittel: ['zwanzig-minuten', 'level-up', 'wortschatz-fokus'],
+    schwer: ['pures-lesen', 'genaues-lesen']
+};
+
+const levelCheckboxes = document.querySelectorAll('input[name="level"]');
+
+function filterLevelCards() {
+    const checkedValues = [...levelCheckboxes].filter(checkbox => checkbox.checked).map(e => e.value);
+    const allCards = document.querySelectorAll('.card');
+
+    allCards.forEach(card => {
+        card.style.display = 'none';
+    });
+
+    if (checkedValues.length === 0) {
+        allCards.forEach(card => {
+            card.style.display = 'flex';
+        });
+
+        const planingAmount = document.getElementById('plan-amount');
+        planingAmount.innerHTML = '';
+        return;
+    }
+
+
+
+    let filteredCardClasses = [];
+   
+
+    for (const value of checkedValues) {
+        const relatedCardClasses = levelCardMap[value];
+
+        if (filteredCardClasses.length === 0) {
+            filteredCardClasses = relatedCardClasses;
+        } else {
+            filteredCardClasses = filteredCardClasses.filter(className => relatedCardClasses.includes(className));
+        }
+    }
+
+    const filteredCards = document.querySelectorAll('.card.' + filteredCardClasses.join(', .card.'));
+    
+    filteredCards.forEach(card => {
+        card.style.display = 'flex';
+    });
+
+
+    const planingAmount = document.getElementById('plan-amount');
+    planingAmount.innerHTML = `<span>(${filteredCards.length})</span>`;
+}
+
+
+levelCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', filterLevelCards);
+});
+
+
 
